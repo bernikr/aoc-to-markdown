3b3a7ef4c584c0b43092b08ab133d91c131f9659
bernikr <berni98k@gmail.com>
2021-12-17 11:02:39 +0100

    refactor structure

--- src/content_scripts/on-loaded.js
+++
@@ -1,9 +0,0 @@
-import { runtime } from "webextension-polyfill";
-
-runtime
-  .sendMessage({
-    action: "showPageAction",
-  })
-  .catch((err) =>
-    console.error("Failed to send 'showPageAction' message", err)
-  );
